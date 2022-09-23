95ee6a40b4e1c1546c6f77d4685840c783c7abe0
Victor Perez <vjperez@mix.wvu.edu>
2022-09-22 21:53:45 -0400

    Updates

---
+++ precache-manifest.2740700130a1017307d9dcc004df42e4.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "47c123fc56746bbb8dd0",
+    "url": "/portfolio/static/css/main.7e175e2f.chunk.css"
+  },
+  {
+    "revision": "47c123fc56746bbb8dd0",
+    "url": "/portfolio/static/js/main.47c123fc.chunk.js"
+  },
+  {
+    "revision": "6982022c15cc623f4ff9",
+    "url": "/portfolio/static/css/1.1bf4eaaa.chunk.css"
+  },
+  {
+    "revision": "6982022c15cc623f4ff9",
+    "url": "/portfolio/static/js/1.6982022c.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "9b6c8da3c489424e2b3e9c9fb6314b37",
+    "url": "/portfolio/static/media/fa-brands-400.9b6c8da3.eot"
+  },
+  {
+    "revision": "48461ea4e797c9774dabb4a0440d2f56",
+    "url": "/portfolio/static/media/fa-brands-400.48461ea4.woff2"
+  },
+  {
+    "revision": "7b464e274bc331f9a765d765359635a5",
+    "url": "/portfolio/static/media/fa-brands-400.7b464e27.woff"
+  },
+  {
+    "revision": "947b9537bc0fecc8130d48eb753495a1",
+    "url": "/portfolio/static/media/fa-brands-400.947b9537.ttf"
+  },
+  {
+    "revision": "381af09a1366b6c2ae65eac5dd6f0588",
+    "url": "/portfolio/static/media/fa-regular-400.381af09a.woff"
+  },
+  {
+    "revision": "7422060ca379ee9939d3b687d072acad",
+    "url": "/portfolio/static/media/fa-regular-400.7422060c.eot"
+  },
+  {
+    "revision": "949a2b066ec37f5a384712fc7beaf2f1",
+    "url": "/portfolio/static/media/fa-regular-400.949a2b06.woff2"
+  },
+  {
+    "revision": "73fe7f1effbf382f499831a0a9f18626",
+    "url": "/portfolio/static/media/fa-regular-400.73fe7f1e.ttf"
+  },
+  {
+    "revision": "b5a61b229c9c92a6ac21f5b0e3c6e9f1",
+    "url": "/portfolio/static/media/fa-regular-400.b5a61b22.svg"
+  },
+  {
+    "revision": "14a08198ec7d1eb96d515362293fed36",
+    "url": "/portfolio/static/media/fa-solid-900.14a08198.woff2"
+  },
+  {
+    "revision": "70e65a7d34902f2c350816ecfe2f6492",
+    "url": "/portfolio/static/media/fa-solid-900.70e65a7d.eot"
+  },
+  {
+    "revision": "0079a0ab6bec4da7d6e16f2a2e87cd71",
+    "url": "/portfolio/static/media/fa-solid-900.0079a0ab.ttf"
+  },
+  {
+    "revision": "815694de1120d6c1e9d1f0895ee81056",
+    "url": "/portfolio/static/media/fa-solid-900.815694de.woff"
+  },
+  {
+    "revision": "b5472631dbace30d549357ec325b9c62",
+    "url": "/portfolio/static/media/fa-brands-400.b5472631.svg"
+  },
+  {
+    "revision": "38508b2e7fac045869a86a15936433f7",
+    "url": "/portfolio/static/media/fa-solid-900.38508b2e.svg"
+  },
+  {
+    "revision": "88e405f99b0c7e550439445513f0e515",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.e8eebb9bad8782123895d96e6fd8700f.js"
+  "/portfolio/precache-manifest.2740700130a1017307d9dcc004df42e4.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/portfolio/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
